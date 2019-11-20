ec56affd8d506aabda25f1f43075cb30f2b6036e
OBON <38969686+OBON-L@users.noreply.github.com>
2019-11-20 20:31:51 +0900

    sizeof(binary) / sizeof(int)는 20이기 때문에 position 변수 사용

--- array/decimal_to_binary_using_arr.c
+++ array/decimal_to_binary_using_arr.c
@@ -1,11 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int main()
-{
-	int num = 37;
+int main() {
+	int num;
+	scanf("%d", &num);
 	int binary[20] = { 0, };
 	int position = 0;
 	
+	// decimal to binary
 	while (1) {
 		binary[position] = num % 2;
 		num /= 2;
@@ -14,10 +16,14 @@ int main()
 
 		position++;
 	}
-
+	printf("decimal to binary : ");
 	for (int i = position; i >= 0; i--) printf("%d", binary[i]);
 
-	printf("\n%d", sizeof(binary) / sizeof(int)); // 20
-	
+	//binary to decimal
+	int decimal = 0;
+	for (int i = 0; i <= position; i++)	decimal += binary[i] << i;
+
+	printf("\nbinary to decimal: %d\n", decimal);
+
 	return 0;
 }
